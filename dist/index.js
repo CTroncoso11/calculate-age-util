f788cf7e4c0dc4a3e26a228baf5a4d69b85ebad4
Cristian Troncoso <cristian.troncoso.lillo@gmail.com>
2021-09-27 06:45:20 -0300

    ✨ Added 'calculate to next birthday' function

--- dist/index.js
+++ dist/index.js
@@ -6,29 +6,39 @@ function checkDayMonth(value) {
         return value.toString();
     }
 }
-function calculateAge(day, month, year) {
+/**
+ * Add two numbers together
+ * @param  {Number} day
+ * @param  {Number} month
+ * @param  {Number} year
+ * @param  {boolean} timeToBirthday If enabled, returns the date values from the actual date to the birthday (must replace the input values with the ones for the next birthday)
+ * @return {Dictionary}  Returns a dictionary with the days, months, years, etc. of the person's life
+ */
+function calculateAge(day, month, year, timeToBirthday) {
     if ((day >= 1 && day <= 31) && (month >= 1 && month <= 12) && (year >= 1 && year <= 9999)) {
         //  Convert date
         var dayStr = checkDayMonth(day);
         var monthStr = checkDayMonth(month);
         var todayDateRaw = new Date();
         var todayDate = new Date((todayDateRaw.getTime() - (todayDateRaw.getTimezoneOffset() * 60000)));
-        var pastYearString = year.toString() + "-" + monthStr + "-" + dayStr + "T00:00:00";
-        var pastYearRaw = new Date(pastYearString);
-        var pastTime = (todayDate.getTime() - pastYearRaw.getTime());
+        var calculateYearString = year.toString() + "-" + monthStr + "-" + dayStr + "T00:00:00";
+        var calculateYearRaw = new Date(calculateYearString);
+        var elapsedTime = timeToBirthday ?
+            (calculateYearRaw.getTime() - todayDate.getTime()) :
+            (todayDate.getTime() - calculateYearRaw.getTime());
         return {
-            seconds: Math.floor(pastTime / 1000),
-            minutes: Math.floor(pastTime / 1000 / 60),
-            hours: Math.floor(pastTime / 1000 / 60 / 60),
-            days: Math.floor(pastTime / 1000 / 60 / 60 / 24),
-            weeks: Math.floor(pastTime / 1000 / 60 / 60 / 24 / 7),
-            month: Math.floor(pastTime / 1000 / 60 / 60 / 24 / 7 / 4.345),
-            years: Math.floor(pastTime / 1000 / 60 / 60 / 24 / 365)
+            seconds: Math.floor(elapsedTime / 1000),
+            minutes: Math.floor(elapsedTime / 1000 / 60),
+            hours: Math.floor(elapsedTime / 1000 / 60 / 60),
+            days: Math.floor(elapsedTime / 1000 / 60 / 60 / 24),
+            weeks: Math.floor(elapsedTime / 1000 / 60 / 60 / 24 / 7),
+            month: Math.floor(elapsedTime / 1000 / 60 / 60 / 24 / 7 / 4.345),
+            years: Math.floor(elapsedTime / 1000 / 60 / 60 / 24 / 365)
         };
     }
     else {
         return false;
     }
 }
-var data = calculateAge(3, 12, 1999);
-console.log(data.month / 12);
+var data = calculateAge(3, 12, 2021, true);
+console.log(data);
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ function checkDayMonth(value: number){
   if( value <= 9 ){ return "0"+value.toString(); } 
   else { return value.toString(); }
 }
-
-function calculateAge(day:number, month:number, year: number) {
+/**
+ * Add two numbers together
+ * @param  {Number} day 
+ * @param  {Number} month 
+ * @param  {Number} year 
+ * @param  {boolean} timeToBirthday If enabled, returns the date values from the actual date to the birthday (must replace the input values with the ones for the next birthday)
+ * @return {Dictionary}  Returns a dictionary with the days, months, years, etc. of the person's life
+ */
+function calculateAge(day:number, month:number, year: number, timeToBirthday: boolean) {
   if((day >= 1 && day <= 31) && (month >= 1 && month <= 12) && (year >= 1 && year <= 9999)) {
     //  Convert date
     let dayStr:string = checkDayMonth(day);
@@ -11,23 +18,26 @@ function calculateAge(day:number, month:number, year: number) {
     
     const todayDateRaw: Date = new Date();
     const todayDate: Date = new Date((todayDateRaw.getTime() - (todayDateRaw.getTimezoneOffset() * 60000)));
-  
-    const pastYearString: string = year.toString()+"-"+monthStr+"-"+dayStr+"T00:00:00";
-    const pastYearRaw: Date = new Date(pastYearString);
     
-    const pastTime = (todayDate.getTime() - pastYearRaw.getTime());
+    const calculateYearString: string = year.toString()+"-"+monthStr+"-"+dayStr+"T00:00:00";
+    const calculateYearRaw: Date = new Date(calculateYearString);
+    
+    const elapsedTime = timeToBirthday ?
+                        (calculateYearRaw.getTime() - todayDate.getTime()) :
+                        (todayDate.getTime() - calculateYearRaw.getTime());
     return {
-      seconds:  Math.floor(pastTime/1000),
-      minutes:  Math.floor(pastTime/1000/60),
-      hours:    Math.floor(pastTime/1000/60/60),
-      days:     Math.floor(pastTime/1000/60/60/24),
-      weeks:    Math.floor(pastTime/1000/60/60/24/7),
-      month:    Math.floor(pastTime/1000/60/60/24/7/4.345), //Average time of how many weeks a month have
-      years:    Math.floor(pastTime/1000/60/60/24/365)
+      seconds:  Math.floor(elapsedTime/1000),
+      minutes:  Math.floor(elapsedTime/1000/60),
+      hours:    Math.floor(elapsedTime/1000/60/60),
+      days:     Math.floor(elapsedTime/1000/60/60/24),
+      weeks:    Math.floor(elapsedTime/1000/60/60/24/7),
+      month:    Math.floor(elapsedTime/1000/60/60/24/7/4.345), //Average time of how many weeks a month have
+      years:    Math.floor(elapsedTime/1000/60/60/24/365)
     }
   } else {
     return false;
   }
 }
-const data:any = calculateAge(3, 12, 1999)
-console.log(data.month/12);
+
+const data:any = calculateAge(3, 12, 2021, true)
+console.log(data);
